733b305d93fa3a3bb9f1b1eab6aca45313517b19
KarimPucci <kizpucci@gmail.com>
2022-10-17 17:57:47 +0200

    main

---
+++ milestone1/script.js
@@ -0,0 +1,4 @@
+/* Per prima cosa, creiamo il markup statico: costruiamo il container e inseriamo un’immagine grande al centro: avremo così la struttura base e gli stili pronti per poterci poi concentrare solamente sull’aspetto logico. */
+
+console.log("hello worldddd");
+
